f979150a0db9bf1b71116c4630257f77f013a7e3
Ravi Ranjan Kumar <ravi.ranjan@wastelink.co>
2026-01-21 17:52:54 +0530

    [ravi]postgresql Database Setup

--- HRMS_Lite/hrms_backend/hrms_lite/wsgi.py
+++ HRMS_Lite/hrms_backend/hrms_lite/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for hrms_lite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hrms_lite.settings')
 
 application = get_wsgi_application()
+
+# ✅ AUTO-RUN MIGRATIONS ON STARTUP (Render Free workaround)
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Migration error:", e)
